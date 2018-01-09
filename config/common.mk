12c0973ba4b9eeb82dcada35af61e6b52b4569b7
Sam Mortimer <sam@mortimer.me.uk>
2018-01-09 01:02:17 -0800

    vendor: Allow tethering without a provisioning app
    Change-Id: Iac67426229aff842f536301edd9ea270d5d3db6e
    Signed-off-by: Mrinal Ghosh <mg712702@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -28,6 +28,11 @@ else
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.adb.secure=1
 endif
 
+# Tethering - allow without requiring a provisioning app
+# (for devices that check this)
+PRODUCT_PROPERTY_OVERRIDES += \
+    net.tethering.noprovisioning=true
+
 # APN
 PRODUCT_COPY_FILES += \
     vendor/legion/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
