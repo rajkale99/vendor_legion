6a586e55bb636b8a9480260c700a60637f0a5907
mdeejay <mdjrussia@gmail.com>
2019-02-08 13:09:49 +0100

    Update bootanimations

--- config/bootanimation.mk
+++ config/bootanimation.mk
@@ -18,6 +18,8 @@ TARGET_BOOT_ANIMATION_RES ?= undefined
 
 ifeq ($(TARGET_BOOT_ANIMATION_RES),1080)
      PRODUCT_COPY_FILES += vendor/beast/prebuilt/common/media/1080.zip:system/media/bootanimation.zip
+else ifeq ($(TARGET_BOOT_ANIMATION_RES),2160)
+     PRODUCT_COPY_FILES += vendor/beast/prebuilt/common/media/2160.zip:system/media/bootanimation.zip
 else ifeq ($(TARGET_BOOT_ANIMATION_RES),undefined)
      $(warning Target bootanimation res is undefined, using generic bootanimation )
      PRODUCT_COPY_FILES += vendor/beast/prebuilt/common/media/bootanimation.zip:system/media/bootanimation.zip
