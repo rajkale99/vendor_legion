5c6b048acda23d67576ca1efc3ae16a9031a446b
mdeejay <mdjrussia@gmail.com>
2019-02-09 12:18:59 +0100

    Add back ro.build.selinux props

--- config/beast_props.mk
+++ config/beast_props.mk
@@ -31,7 +31,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.disable_rescue=true \
     ro.config.calibration_cad=/system/etc/calibration_cad.xml \
     persist.debug.wfd.enable=1 \
-    persist.sys.wfd.virtual=0
+    persist.sys.wfd.virtual=0 \
+    ro.build.selinux=0
 
 ifneq ($(TARGET_BUILD_VARIANT),user)
 # Thank you, please drive thru!
