1b67f6fca5f73b35a01f7ba70fe2be0edbca3601
Rajkale99 <kaleraj.rk@gmail.com>
2019-02-28 17:05:51 +0100

    Update bootanimation

--- config/bootanimation.mk
+++ config/bootanimation.mk
@@ -18,8 +18,6 @@ TARGET_BOOT_ANIMATION_RES ?= undefined
 
 ifeq ($(TARGET_BOOT_ANIMATION_RES),1080)
      PRODUCT_COPY_FILES += vendor/legion/prebuilt/common/media/1080.zip:system/media/bootanimation.zip
-else ifeq ($(TARGET_BOOT_ANIMATION_RES),2160)
-     PRODUCT_COPY_FILES += vendor/legion/prebuilt/common/media/2160.zip:system/media/bootanimation.zip
 else ifeq ($(TARGET_BOOT_ANIMATION_RES),undefined)
      $(warning Target bootanimation res is undefined, using generic bootanimation )
      PRODUCT_COPY_FILES += vendor/legion/prebuilt/common/media/bootanimation.zip:system/media/bootanimation.zip
