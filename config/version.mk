6a1ae966742a2791e27a3250137e78166a39b54d
rajkale99 <kaleraj.rk@gmail.com>
2019-05-17 15:52:13 +0530

    Bump To legion-1.3 also update prebuilt google dialer

--- config/version.mk
+++ config/version.mk
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-LEGION_VERSION = 1.2
+LEGION_VERSION = 1.3
 
 ifndef LEGION_BUILD_TYPE
     LEGION_BUILD_TYPE := UNOFFICIAL
