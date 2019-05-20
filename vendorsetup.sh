98566d13d2a0d1c601ce63dab909af4893bb6170
rajkale99 <kaleraj.rk@gmail.com>
2019-05-20 10:33:59 +0000

    Add Legion Tools Taken from ColtOS

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,4 @@
-add_lunch_combo legion_$devices-userdebug
+#LegionOS
+for device in $(curl -s https://raw.githubusercontent.com/legionRom/platform_vendor_legion/pie/legion.devices); do
+add_lunch_combo legion_$device-userdebug
+done
