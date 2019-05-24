12ecd3cfbb75a8ce19b2422aef6953dc679251ef
Rajkale99 <kaleraj.rk@gmail.com>
2019-05-24 16:33:29 +0530

    Merge legion-1.4 (#8)
    * Build SettingsSuggestions
    
    This fixed the search fc in settings
    
    * Updated to 1.4
    
    * Revert "Add Legion Tools"
    
    This reverts commit 98566d13d2a0d1c601ce63dab909af4893bb6170.

--- config/version.mk
+++ config/version.mk
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-LEGION_VERSION = 1.3
+LEGION_VERSION = 1.4
 
 ifndef LEGION_BUILD_TYPE
     LEGION_BUILD_TYPE := UNOFFICIAL
