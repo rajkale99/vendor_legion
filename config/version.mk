7419e923ac5ae2d658c3e3f0607ca0699a0c2e49
rajkale99 <kaleraj.rk@gmail.com>
2019-06-26 16:40:52 +0000

    Bump to legion-1.6

--- config/version.mk
+++ config/version.mk
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-LEGION_VERSION = 1.5
+LEGION_VERSION = 1.6
 
 ifndef LEGION_BUILD_TYPE
     LEGION_BUILD_TYPE := UNOFFICIAL
