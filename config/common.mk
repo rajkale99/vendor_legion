1c8ce591fd37eb45fbcaac04ce05153617158385
rajkale99 <kaleraj.rk@gmail.com>
2020-01-12 12:12:58 +0530

    fix derp

--- config/common.mk
+++ config/common.mk
@@ -173,7 +173,7 @@ PRODUCT_COPY_FILES += \
 # Lawnchair
 PRODUCT_COPY_FILES += \
     vendor/legion/prebuilt/common/etc/permissions/privapp-permissions-lawnchair.xml:system/etc/permissions/privapp-permissions-lawnchair.xml \ 
-    vendor/legion/prebuilt/common/etc/sysconfig/lawnchair-hiddenapi-package-whitelist.xml:system/etc/sysconfig/lawnchair-hiddenapi-package-whitelis$
+    vendor/legion/prebuilt/common/etc/sysconfig/lawnchair-hiddenapi-package-whitelist.xml:system/etc/sysconfig/lawnchair-hiddenapi-package-whitelist.xml
 
 # Inclusion of legion specific files
 
