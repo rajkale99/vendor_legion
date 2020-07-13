theOverpoweredGuy/android_vendor_zenx

vendor: Guard faceunlock
Change-Id: I58aedaa815064ca9680d175befada87e187305ec

config/common.mk
@@ -265,6 +265,7 @@ PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/zenx/overlay
 DEVICE_PACKAGE_OVERLAYS += vendor/zenx/overlay/common
 
 # Face Unlock
+ifeq ($(ZENX_BUILD_TYPE), Official)
 TARGET_FACE_UNLOCK_SUPPORTED := true
 ifneq ($(TARGET_DISABLE_ALTERNATIVE_FACE_UNLOCK), true)
 PRODUCT_PACKAGES += \
@@ -273,6 +274,7 @@ TARGET_FACE_UNLOCK_SUPPORTED := true
 endif
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.face.moto_unlock_service=$(TARGET_FACE_UNLOCK_SUPPORTED)
+endif
 
 # Enable ccache
 USE_CCACHE := true

rajkale99/vendor_legion

config/common.mk
@@ -254,6 +254,7 @@ PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/legion/overlay
 -include vendor/legion/gapps.mk
 
 # Face Unlock
+ifeq ($(LEGION_BUILD_TYPE), Official)
 TARGET_FACE_UNLOCK_SUPPORTED := true
 ifneq ($(TARGET_DISABLE_ALTERNATIVE_FACE_UNLOCK), true)
 PRODUCT_PACKAGES += \
@@ -262,6 +263,7 @@ TARGET_FACE_UNLOCK_SUPPORTED := true
 endif
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.face.moto_unlock_service=$(TARGET_FACE_UNLOCK_SUPPORTED)
+endif
 
 # Enable ccache
 USE_CCACHE := true