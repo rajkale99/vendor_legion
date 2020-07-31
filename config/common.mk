Project-Xtended/vendor_xtended

vendor: update ovrlays & permissions for device personalizations
SDB: taken from PixelGapps of PE & Flame Gapps

Change-Id: Ia4141dca993522eea81a0a2bb618489df4e29d83

config/common.mk
@@ -88,6 +88,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/xtended/config/permissions/xtended-sysconfig.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/xtended-sysconfig.xml
 
+# Device Personalization Services Permission
+PRODUCT_COPY_FILES += \
+    vendor/xtended/config/permissions/com.google.android.as.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/com.google.android.as.xml \
+    vendor/xtended/config/permissions/com.google.android.as.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.google.android.as.xml
+
 # Copy all Xtended-specific init rc files
 $(foreach f,$(wildcard vendor/xtended/prebuilt/common/etc/init/*.rc),\
 	$(eval PRODUCT_COPY_FILES += $(f):$(TARGET_COPY_OUT_SYSTEM)/etc/init/$(notdir $f)))

rajkale99/vendor_legion

config/common.mk
@@ -80,7 +80,12 @@ PRODUCT_COPY_FILES += \
 
 # Copy all Legion-specific init rc files
 $(foreach f,$(wildcard vendor/legion/prebuilt/common/etc/init/*.rc),\
-	$(eval PRODUCT_COPY_FILES += $(f):$(TARGET_COPY_OUT_SYSTEM)/etc/init/$(notdir $f)))
+        $(eval PRODUCT_COPY_FILES += $(f):$(TARGET_COPY_OUT_SYSTEM)/etc/init/$(notdir $f)))
+
+# Device Personalization Services Permission
+PRODUCT_COPY_FILES += \
+    vendor/legion/config/permissions/com.google.android.as.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/com.google.android.as.xml \
+    vendor/legion/config/permissions/com.google.android.as.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.google.android.as.xml
 
 # Copy over added mimetype supported in libcore.net.MimeUtils
 PRODUCT_COPY_FILES += \